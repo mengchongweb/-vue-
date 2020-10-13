05355c0b4bb9d8410f35ca35e1b38dc22908cb49
mengchongweb <15350702743@163.com>
2020-10-13 16:11:57 +0800

    todoList-vueCli练习

---
+++ todolist/src/components/TodoItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li class="item" @click='handleDelete'>{{content}}</li>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['content', 'index'],
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    handleDelete () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+<!-- 加scoped -->
+<style scoped>
+  .item{
+    color:red;
+  }
+
+</style>
